4de59b9b5e1c19158ff3d7d20c1ffa07a8ff8c4c
Victor Emmanuel <vicbem@hotmail.com>
2022-10-18 00:21:59 -0300

    Wall Slide add

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,16 +6,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    private enum Facing
+    {
+        Left,
+        Right
+    }
+
     [Header("Player Properties")]
     [SerializeField] private int life = 2;
     [SerializeField] private float playerSpeed = 2.0f;
     [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float wallSlidingSpeed = 1.0f;
     private Rigidbody rb;
     
-    [Header("Ground Collision")]
+    [Header("Collisions")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask ground;
     [SerializeField] private float sizeGroundCheck = 1.0f;
+    [SerializeField] private Transform inFront;
 
     [Header("ColdDowns")]
     [SerializeField] private float coldDownWallJumping = 1;
@@ -27,7 +35,9 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float coldDownInvincibility = 1;
     private bool isInvincible;
-    
+
+    private Facing actualFacing = Facing.Right;
+
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
@@ -39,12 +49,28 @@ public class Player : MonoBehaviour
         Move();
     }
 
+    private void FixedUpdate()
+    {
+        CheckInFromWallColliding();
+    }
+
+    private void ChangeDirection()
+    {
+        if (rb.velocity.x > 0 && actualFacing == Facing.Left || rb.velocity.x < 0 && actualFacing == Facing.Right)
+        {
+            gameObject.transform.Rotate(0.0f, 180.0f, 0.0f, Space.World);
+            actualFacing = actualFacing == Facing.Left ? Facing.Right : Facing.Left;
+        }
+    }
+    
     private void Move()
     {
         var horizontalInput = Input.GetAxis("Horizontal");
         var verticalInput = Input.GetAxis("Vertical");
-
+        
         rb.velocity = new Vector3(horizontalInput * playerSpeed, rb.velocity.y, verticalInput * playerSpeed);
+        
+        ChangeDirection();
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
@@ -100,18 +126,28 @@ public class Player : MonoBehaviour
             yield return new WaitForSeconds(flickFrequency);
         }
     }
-    
-    private void OnCollisionStay(Collision collision)
+
+    private void CheckInFromWallColliding()
     {
-        if (!IsGrounded() && collision.contacts[0].normal.y < 0.1f)
+        RaycastHit hit;
+        var inFrontHit = Physics.Raycast(inFront.position, inFront.TransformDirection(Vector3.forward), out hit, 0.1f, ground);
+        if (!IsGrounded() && inFrontHit && hit.normal.y < 0.1f)
         {
             if (Input.GetButton("Jump") && canWallJump)
             {
-                Debug.DrawRay(collision.contacts[0].point, collision.contacts[0].normal, Color.red, 1.25f);
-                rb.AddForce(new Vector3(collision.contacts[0].normal.x * 8, 0, 0), ForceMode.Impulse);
-                rb.velocity = new Vector3(collision.contacts[0].normal.x * 8, jumpHeight, rb.velocity.z);
+                rb.AddForce(new Vector3(hit.normal.x * 8, 0, 0), ForceMode.Impulse);
+                rb.velocity = new Vector3(hit.normal.x * 8, jumpHeight, rb.velocity.z);
                 canWallJump = false;
                 Invoke(nameof(SetWallJumpTrue), coldDownWallJumping);
+            }
+
+            var inputDirection = Input.GetAxis("Horizontal"); 
+            var playerGoingToWall = inputDirection >= 0 && hit.normal.x < 0 || inputDirection < 0 && hit.normal.x >= 0; 
+            
+            if (Input.GetButton("Horizontal") && playerGoingToWall && !Input.GetButton("Jump"))
+            {
+                var velocity = rb.velocity;
+                rb.velocity = new Vector3(velocity.x, Mathf.Clamp(velocity.y, -wallSlidingSpeed, float.MaxValue), velocity.z);
             }
         }
     }
